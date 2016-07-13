414a1861f1522a5769a3731155fbe6a573bc3317
JonathanR <jonathan1031r@gmail.com>
2016-07-13 10:52:06 -0400

    commit erryday

--- week5/day1/jQuery.js
+++ week5/day1/jQuery.js
@@ -3,17 +3,44 @@ $(document).ready(function(){
 	$(".js-character-form").on("submit", createCharacters);
 
 	$(".js-fetch-characters").no("click", fetchCharacters);
-})
+});
 
 function createCharacters (event){
 	event.preventDefault();
 
-	alert("FORM SUBMITTED")
+	var name = $('[name="name"]').val();
+	var occupation = $('[name="occupation"]').val();
+	var weapon = $('[name="weapon"]').val();
+
+$(".js-character-list").prepend(listContent)
 }
 
 function fetchCharacters (){
-	alert("FETCH CHARACTERS")
-} 
+	$.ajax({
+		url: "http://ironhack-characters.herokuapp.com/characters", 
+		success: showCharacters,
+		error: handleError 
+	});
+}	
+
+function showCharacters(response){	
+	console.log(response);
+
+	var charactersArray = response;
+
+	charactersArray.forArray(function (thecharacter){
+
+	var listContent = 
+	<li>
+		<h3> ${thecharacter.name} </h3>
+		
+		<ul>
+			<li> Occupation: ${thecharacter.occupation} </li>
+			<li> Weapon: ${thecharacter.weapon} </li>
+		</ul>
+	</li>
+
+})}
 
 
 
@@ -26,4 +53,4 @@ function fetchCharacters (){
 
 
 
-// $.ajax({url: "http://ironhack-characters.herokuapp.com/characters", success: function (response) {console.log(response.all)} });
+$.ajax({url: "http://ironhack-characters.herokuapp.com/characters", success: function (response) {console.log(response.all)} });
