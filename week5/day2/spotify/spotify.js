ca0c4ef658b29eb6b4579d61c8d443fb7ebb9d94
JonathanR <jonathan1031r@gmail.com>
2016-07-14 09:21:21 -0400

    added albums to spotify

--- week5/day2/spotify/spotify.js
+++ week5/day2/spotify/spotify.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
 	$(".theForm").on("submit", function(event){
-	event.preventDefault();	
-	theSearch()	
-	// $("img").on("click", showAlbum)
+		event.preventDefault();	
+		theSearch()	
+	$('.body-album').on('show.bs.modal', function (e) {
+		if (!data) return e.preventDefault() // stops modal from being shown
+		})
 	});
 });
 
+var newArtist = $(".theInput").val()
+
 function theSearch (){
 	
-
-	var newArtist = $(".theInput").val()
-
 	$.ajax({
 		url: `https://api.spotify.com/v1/search?type=artist&query=${newArtist}`, 
 		success: showArtist,
@@ -20,27 +21,41 @@ function theSearch (){
 }
 
 function showArtist (response){
-	$("theForm").empty();
 
-	console.log(response)
+//  ---when button clicked directs to grab albums---------
+	// $.ajax({ 
+	// 	url: `https://api.spotify.com/v1/albums/${albumsIds}`,
+	// 	success: showAlbum,
+	// 	error: handleError 
+	// });
+
+//	$("theInput").empty();
+
+//----testing whats inside--->	console.log(response)
 
  	response.artists.items.forEach(function (artist){
-		console.log(artist.name);
-		console.log(artist.images.url);
-
+//----testing retutrn-->	console.log(artist.name);
+//----testing return-->		console.log(artist.id);
 		var name = artist.name;
+		var id = artist.id;
 
-		// var images = artist.images[0].url;
-		
+//---Not all objects had pictures ---- or more than just one ---------------------------vvvvvvv
 		if (artist.images.length > 0){
 			$("#list").append(`<h1> ${name} </h1>`);
-			$("#list").append(`<img width="300" heigth="300" src=${artist.images[0].url}>`);
+			$("#list").append(`<img width="300" heigth="300" data-id="${albums}" src=${artist.images[0].url}>`);
 		}
+ 	})
+
+	function showAlbum(response){
+		console.log(response)
 
 
-
- 	}
- )};
+		("img").onclick
+			$(".modal").append("albums")
+			$('.modal').modal("show") 
+		
+	}	
+};
 
 
 
@@ -48,3 +63,13 @@ function handleError (error) {
   console.log("Oh no! There was an error.");
   console.log(error.responseText);
 }
+
+
+
+
+
+// someimage.onclick $('.modal').modal("show")
+//
+// 
+
+
